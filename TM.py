7da6fcd4473450446894e8089b9fa497be89963c
Joe Bernert <joebernert@gmail.com>
2020-10-26 02:40:59 +0000

    cleaned up

--- TM.py
+++ TM.py
@@ -1,19 +1,21 @@
 import sys
-
+import random
 
 def thue_morse_digits(digits):
     return [bin(n).count('1') % 2 for n in range(digits)]
 
-
 def alternating_digits(digits):
     return [n % 2 for n in range(digits)]
 
+def random_digits(digits):
+	return [random.randint(0,1) for n in range(digits)]
 
 def ListXOR(l1, l2):
     x = []
     for n in range(len(l1)):
         x.append(int((l1[n] != l2[n])))
     return x
+
 
 
 ##a = thue_morse_digits(int(sys.argv[1]))
--- script.py
+++ script.py
@@ -1,22 +1,36 @@
-from oracle import Model
 import TM
-m = Model('10', 5)
-a = TM.thue_morse_digits(1000)
-inputstr = ''.join(str(e) for e in a)
-b = []
-for x in inputstr:
-	m.feed(x)
-	b.append(m.guess())
-c = (zip(inputstr,b))
+import sys
+import random
 
-d = []
-for y in c:
-	if y[0] == y[1]:
-		d.append(1)
-	else:
-		d.append(0)
+def do(a):
+
+    from oracle import Model
+    m = Model('10', 8)
+    init = (TM.random_digits(20))
+    initfeed = ''.join(str(e) for e in init)
+    m.feed(initfeed)
+    inputstr = ''.join(str(e) for e in a)
+    b = []
+    for x in inputstr:
+        b.append(m.guess())
+        m.feed(x)
+    c = (zip(inputstr, b))
+
+    d = []
+    for y in c:
+        if y[0] == y[1]:
+            d.append(1)
+        else:
+            d.append(0)
+
+##    print(zip(c, d))
+    ##	print (d)
+    print(d.count(1))
 
 
-print (zip (c,d))
-
-print (d.count(1))
+a = (TM.thue_morse_digits(int(sys.argv[1])))
+do (a)
+rs = random.sample (a, k=len(a)) 
+do (rs)
+do(TM.random_digits(int(sys.argv[1])))
+do(TM.alternating_digits(int(sys.argv[1])))
