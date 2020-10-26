743a5038473ff4a7ffbe3cf02f64ac6a8928e90e
Joe Bernert <joebernert@gmail.com>
2020-10-26 01:56:33 +0000

    Added accuracy count

--- script.py
+++ script.py
@@ -1,9 +1,22 @@
 from oracle import Model
 import TM
 m = Model('10', 5)
-a = TM.thue_morse_digits(100)
+a = TM.thue_morse_digits(1000)
 inputstr = ''.join(str(e) for e in a)
-m.feed(inputstr)
-print (m.guess())
+b = []
+for x in inputstr:
+	m.feed(x)
+	b.append(m.guess())
+c = (zip(inputstr,b))
+
+d = []
+for y in c:
+	if y[0] == y[1]:
+		d.append(1)
+	else:
+		d.append(0)
 
 
+print (zip (c,d))
+
+print (d.count(1))
